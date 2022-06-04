9e283a23a5b6dac1078b321c8d53b547b757f2ae
Jeff Zohrab <jzohrab@gmail.com>
2022-06-04 14:22:17 -0700

    Less noisy deploy.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,9 +4,10 @@
 source ./deploy/deploy.config
 
 mkdir -p "${DESTDIR}/anki-backlog"
-cp -R . "${DESTDIR}/anki-backlog"
+cp -R . "${DESTDIR}/anki-backlog" 2>&1 > /dev/null
 
 pushd "${DESTDIR}/anki-backlog"
+rm -rf .git
 rm -rf deploy
 popd
 
